9db988d71879ec27072192f9643b3a65b4cc9d49
David Torres <djjabbua@gmail.com>
2020-02-05 12:06:42 -0500

    added files

---
+++ code/intro.js
@@ -0,0 +1,8 @@
+// Intro slider en bootstrap
+
+// window.onload = function(){
+//     startGame()
+    
+// }
+
+// opening and calling an image in first js document means that it can be then used in following js files as long as they are called after, reducing lag.
