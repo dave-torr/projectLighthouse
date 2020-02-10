3655693d66e4a16499ca873056ff5c178d468feb
David Torres <djjabbua@gmail.com>
2020-02-09 23:50:17 -0500

    added files

---
+++ code2/landing/page.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+//     $("#myCarousel").on('slid.bs.carousel', function () {
+//       alert('Finished sliding');
+//     });
+//  });
+
+        document.onkeydown = function(e) { 
+        switch (e.keyCode) { 
+            case 32: document.link("/module1/project1/game.html");   
+            break;
+        } }
+
+// let mySound;
+
+// function startGame() {
+//   mySound = new sound(thunderstorm-sound-effect.mp3);
+//   myGameArea.start();
+// }
+
+// function updateGameArea() {
+//       mySound.play();
+//       myGameArea.stop();
+//     }
+  
